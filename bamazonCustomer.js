f8a550d0a282d0623fe9300edf53eff5aa3624b7
Gouri <gouri.josh@gmail.com>
2019-11-13 23:10:54 -0800

    created the logic for purchase order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var asTable = require ('as-table')
+var asTable = require('as-table')
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -23,16 +23,68 @@ connection.connect(function (err) {
     start();
 });
 
-function start(){
+function start() {
 
     var query = connection.query("select * from products", function (err, res) {
         if (err) throw err;
-        console.log(asTable.configure ({ delimiter: ' | ' }) (res));
-        
+        console.log(asTable.configure({ delimiter: ' | ' })(res));
+
+        inquirer
+            .prompt([
+                {
+                    name: "buy_item_id",
+                    type: "list",
+                    message: "Enter the id of the product you want to buy",
+                    choices: res.map(x => x.item_id)
+
+                },
+                {
+                    name: "buy_item_quantity",
+                    type: "input",
+                    message: "Enter the quantity of the product"
+                }
+            ]).then(function (answer) {
+
+                console.log(answer.buy_item_id + ": " + answer.buy_item_quantity);
+                var product = res.find(x => x.item_id === answer.buy_item_id);
+                console.log(product);
+
+                if (product.stock_quantity >= answer.buy_item_quantity) {
+                    //update the product
+                    var quantity=product.stock_quantity-answer.buy_item_quantity;                    
+                    console.log("Updating the stock quantity to: "+ quantity);
+                    
+                    var query=connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: quantity
+                            },
+                            {
+                                item_id: answer.buy_item_id
+                            }
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + " products updated!\n");
+                           
+                        }
+                    );
+
+                    // logs the actual query being run
+                    //console.log(query.sql);
+                     console.log(query.sql);
+                     connection.end();
+
+                }
+                else {
+                    console.log("Insufficient quantity!");
+                }
+            });
+
     });
 
     // logs the actual query being run
     console.log(query.sql);
-    connection.end();
 }
 
